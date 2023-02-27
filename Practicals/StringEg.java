663e40d0cdaff7779e8d896fd3050aaa1eafe567
ApurvaGawde <123156656+ApurvaGawde@users.noreply.github.com>
2023-02-27 17:40:18 +0530

    Add files via upload

--- Practicals/StringEg.java
+++ Practicals/StringEg.java
@@ -6,16 +6,29 @@ public class StringEg {
 		String a3 = new String("anudip");
 		String a2 = "Anudip Foundatiom Vashi";
 		String a4 = "Anudip";
+		
 		//equals() compares values of string
 		System.out.println(a1.equals(a2));//compares values of string
 		System.out.println(a1.equals(a3));//case mismatch
 		System.out.println(a1.equalsIgnoreCase(a3));
 		
-		//== compares values case sensitive
-		
+		//== compares (l.h.s to r.h.s) values case sensitive
 		System.out.println(a1==a2);
 		System.out.println(a1==a4);
-
+		
+		//lexicographically(Alphabetical order)
+		String a5 = "apple";
+		String a6 = "america";
+		System.out.println(a6.compareTo(a5));//displays negative no(a6<a5)
+		System.out.println(a1.compareTo(a4));//equal 0
+		System.out.println(a2.compareTo(a1));//displays positive no(a2>a1)
+		
+		//concatenation
+		System.out.println(a5+ " "+a6);
+		System.out.println(60+70+"my"+90+74);//before string (addition operation is performed) after that only concatenation
+		System.out.println(a5.concat(a6));
 	}
+
+	
 
 }
